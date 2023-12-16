f6c23d924129c51733a1b0baeccea47be4d6d331
Dynamic Bineuro <dynamic.bineuro@gmail.com>
2023-12-15 20:55:06 -0800

    Changed order to something more sensical :D

--- Actor.h
+++ Actor.h
@@ -2,12 +2,12 @@
 class Actor {
 public:
     virtual void OnCreate() = 0;
-    virtual void OnDestroy() = 0;
     virtual void OnStart() = 0;
     virtual void OnUpdate(float deltaTime) = 0;
     virtual void OnFixedUpdate(float fixedDeltaTime) = 0;
     virtual void OnEnable() = 0;
     virtual void OnDisable() = 0;
+    virtual void OnDestroy() = 0;
 
     virtual ~Actor() {} // Ensure a virtual destructor for derived classes
 };
